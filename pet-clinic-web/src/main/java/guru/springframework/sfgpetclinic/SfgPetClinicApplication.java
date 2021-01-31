fd6f8c5d949742e78af8f0569613504c8c196576
BillyY1 <billy.yang@verifone.com>
2021-01-31 01:24:00 -0500

    [billy] BRANCH info update

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,7 @@
 package guru.springframework.sfgpetclinic;
+/**
+ * crud test pet map #62
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
