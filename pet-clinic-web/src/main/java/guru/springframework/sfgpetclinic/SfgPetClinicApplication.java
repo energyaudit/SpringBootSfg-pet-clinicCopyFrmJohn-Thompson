7c73cf0602d614348a22125e6d40da9796955f1f
BillyY1 <billy.yang@verifone.com>
2021-02-07 13:05:45 -0500

    [billy] BRANCH info update

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,7 @@
 package guru.springframework.sfgpetclinic;
+/**
+ * close #56 add builder pattern #56 petType sfg-pet-clinic-03b09b
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
