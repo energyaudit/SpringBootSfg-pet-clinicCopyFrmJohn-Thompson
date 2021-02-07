37651d17340add2c3fbd0e0d2469bb6b3745af18
BillyY1 <billy.yang@verifone.com>
2021-02-07 17:42:07 -0500

    [billy] BRANCH info update

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,7 @@
 package guru.springframework.sfgpetclinic;
+/**
+ * add mock mvc test # 52 sfg-pet-clinic-e9c63
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
